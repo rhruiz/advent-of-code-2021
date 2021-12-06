586cb86c809ea5aae86c39aaef703dacfd1e3b9c
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2021-12-06 19:42:57 -0300

    Day 6, second star

---
+++ lib/06/second.ex
@@ -0,0 +1,41 @@
+defmodule Aoc2021.Day6.Second do
+  def run(file, days) do
+    initial = Enum.into(0..8, %{}, fn day -> {day, 0} end)
+
+    input =
+      file
+      |> input()
+      |> Enum.frequencies()
+
+    inital
+    |> Map.merge(input)
+    |> simulate(days)
+  end
+
+  def simulate(population, days) do
+    simulate(population, days, 0)
+  end
+
+  def simulate(population, days, days) do
+    population |> Map.values() |> Enum.sum()
+  end
+
+  def simulate(population, days, day) do
+    new_population =
+      Enum.reduce(1..8, %{}, fn age, new_population ->
+        Map.put(new_population, age - 1, population[age])
+      end)
+
+    new_population
+    |> Map.update!(6, fn count -> count + population[0] end)
+    |> Map.put(8, population[0])
+    |> simulate(days, day + 1)
+  end
+
+  def input(file) do
+    file
+    |> File.read!()
+    |> String.split(",", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+end
--- test/06_test.exs
+++ test/06_test.exs
@@ -8,7 +8,19 @@ defmodule Aoc2021.Day6Test do
     end
 
     test "with puzzle data" do
-      assert 380612 = Enum.count(First.run(input(), 80))
+      assert 380_612 = Enum.count(First.run(input(), 80))
+    end
+  end
+
+  describe "second star" do
+    test "with test data" do
+      assert 26 = Second.run(test_input(), 18)
+      assert 5934 = Second.run(test_input(), 80)
+      assert 26_984_457_539 = Second.run(test_input(), 256)
+    end
+
+    test "with puzzle data" do
+      assert 1_710_166_656_900 = Second.run(input(), 256)
     end
   end
 end
