e04c08a43557ef8877d2191f8f833e23a74ff2d4
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2021-12-07 19:51:32 -0300

    Day 7, second star

---
+++ lib/07/second.ex
@@ -0,0 +1,47 @@
+defmodule Aoc2021.Day7.Second do
+  def run(file) do
+    crabs =
+      file
+      |> input()
+
+    cg = div(Enum.sum(crabs), length(crabs))
+
+    position = minimal(crabs, cg)
+    distance(crabs, position)
+  end
+
+  def minimal(crabs, candidate) do
+    minimal(crabs, candidate, distance(crabs, candidate), %{})
+  end
+
+  def minimal(crabs, candidate, best, cache) do
+    to_left = distance(crabs, candidate - 1)
+    to_right = distance(crabs, candidate + 1)
+
+    cond do
+      to_left < to_right && to_left < best ->
+        minimal(crabs, candidate - 1, to_left, cache)
+
+      to_right < to_left && to_right < best ->
+        minimal(crabs, candidate + 1, to_right, cache)
+
+      true ->
+        candidate
+    end
+  end
+
+  def distance(crabs, position) do
+    Enum.reduce(crabs, 0, fn crab, distance ->
+      steps = trunc(abs(position - crab))
+
+      distance + steps * (1 + steps) / 2
+    end)
+  end
+
+  def input(file) do
+    file
+    |> File.read!()
+    |> String.split(",", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+end
--- test/07_test.exs
+++ test/07_test.exs
@@ -10,4 +10,14 @@ defmodule Aoc2021.Day7Test do
       assert 336_701 = First.run(input())
     end
   end
+
+  describe "second star" do
+    test "with test data" do
+      assert 168.0 = Second.run(test_input())
+    end
+
+    test "with puzzle data" do
+      assert 95_167_302.0 = Second.run(input())
+    end
+  end
 end
