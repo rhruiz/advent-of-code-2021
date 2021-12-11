f77d5078d310ab6c775e8528dc9d08e8f49d1c18
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2021-12-11 11:55:05 -0300

    Day 11, first star

---
+++ test/11_test.exs
@@ -0,0 +1,14 @@
+defmodule Aoc2021.Day11Test do
+  use TestCase, async: true
+
+  describe "first star" do
+    test "with test data" do
+      assert 1656 = First.run(test_input())
+    end
+
+    test "with puzzle data" do
+      assert 1608 = First.run(input())
+    end
+  end
+end
+
