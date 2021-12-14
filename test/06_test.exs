3fdd9b311f2a6440d870bf35056c5b60fef201eb
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2021-12-14 20:26:52 -0300

    Use second star to solve first in day 6

--- test/06_test.exs
+++ test/06_test.exs
@@ -3,12 +3,12 @@ defmodule Aoc2021.Day6Test do
 
   describe "first star" do
     test "with test data" do
-      assert 26 = Enum.count(First.run(test_input(), 18))
-      assert 5934 = Enum.count(First.run(test_input(), 80))
+      assert 26 = Second.run(test_input(), 18)
+      assert 5934 = Second.run(test_input(), 80)
     end
 
     test "with puzzle data" do
-      assert 380_612 = Enum.count(First.run(input(), 80))
+      assert 380_612 = Second.run(input(), 80)
     end
   end
 
