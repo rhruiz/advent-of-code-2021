bd27f84d2b2d424d52cd4a1274807a5e21e69329
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2021-12-16 00:19:21 -0300

    mix format all the things

--- test/15_test.exs
+++ test/15_test.exs
@@ -3,7 +3,7 @@ defmodule Aoc2021.Day15Test do
 
   describe "first star" do
     test "with test data" do
-      assert 40  = First.run(test_input())
+      assert 40 = First.run(test_input())
     end
 
     test "with puzzle data" do
@@ -11,20 +11,20 @@ defmodule Aoc2021.Day15Test do
     end
   end
 
-   describe "second star" do
-     test "expand with test data" do
-       {map, {xmax, ymax}} = Second.input(test_input())
-       {map, {xmax, ymax}} = Second.expand(map, {xmax, ymax})
+  describe "second star" do
+    test "expand with test data" do
+      {map, {xmax, ymax}} = Second.input(test_input())
+      {map, {xmax, ymax}} = Second.expand(map, {xmax, ymax})
 
-       assert {map, {xmax, ymax}} == Second.input("test/support/15/big_test_input.txt")
-     end
+      assert {map, {xmax, ymax}} == Second.input("test/support/15/big_test_input.txt")
+    end
 
-     test "with test data" do
-       assert 315 = Second.run(test_input())
-     end
+    test "with test data" do
+      assert 315 = Second.run(test_input())
+    end
 
-     test "with puzzle data" do
-       assert 3045 = Second.run(input())
-     end
-   end
+    test "with puzzle data" do
+      assert 3045 = Second.run(input())
+    end
+  end
 end
